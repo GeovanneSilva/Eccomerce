556277385f69c58048b9c821f2a4da58345017d5
Geovanne Silva <geovanne.08@gmail.com>
2023-01-06 21:16:00 +0000

    code upload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <navbar class="index" />
+    <dashboard />
+    <Footer></Footer>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Dashboard from '../components/Dashboard.vue';
+import Navbar from '../components/Navbar.vue';
+import Footer from '../components/Footer.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Dashboard,
+    Navbar,
+    Footer,
   },
 };
 </script>
